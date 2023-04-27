86f1660f2853a54f4c89f53014cb00a339cd7ce4
Hank Kim <hank29206880@gmail.com>
2023-04-27 17:01:03 +0900

    ts-blockchain

---
+++ build/myPackage.js
@@ -0,0 +1,17 @@
+//@ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @returns {boolean}
+ */
+export function init(config) {
+    return true;
+}
+/**
+ * return exit code
+ * @param {number} code
+ * @returns {number}
+ */
+export function exit(code) {
+    return code + 1;
+}
